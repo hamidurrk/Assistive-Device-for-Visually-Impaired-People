e61cde28b95bbe78162852ea50862c4eaf44dbb0
hamidurrk <hamidurrk@gmail.com>
2022-09-30 05:54:40 +0600

    printBraille working

---
+++ test/vib_motors.ino
@@ -0,0 +1,28 @@
+
+#define dot1 23
+#define dot2 22
+#define dot3 21
+#define dot4 19
+#define dot5 18
+#define dot6 5
+
+
+
+void setup() {
+    pinMode(dot1, OUTPUT);
+    pinMode(dot2, OUTPUT);
+    pinMode(dot3, OUTPUT);
+    pinMode(dot4, OUTPUT);
+    pinMode(dot5, OUTPUT);
+    pinMode(dot6, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(dot1, HIGH);
+  digitalWrite(dot2, HIGH);
+  digitalWrite(dot3, HIGH);
+  digitalWrite(dot4, HIGH);
+  digitalWrite(dot5, HIGH);
+  digitalWrite(dot6, HIGH);   
+                                           
+}
